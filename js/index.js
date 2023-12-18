c77adec26b68c8d4371094ed3c8bcf0fbcf53d11
Miriam Völkl <miriamvoelkl@gmail.com>
2023-12-18 15:46:18 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -2,6 +2,15 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  console.log("r2d2s eyes: ", data.results[2].eye_color);
+  const allNames = data.results.map((character) => character.name);
+  console.log("all names: ", allNames);
+  return data;
+}
 
 fetchData();
